26ca3b05ab5be93c8bf6f060033b5c87d26606bc
PoornimaKarma <161825445+PoornimaKarma@users.noreply.github.com>
2024-05-10 16:46:46 +0530

    Update AttendenceService.java

--- src/services/AttendenceService.java
+++ src/services/AttendenceService.java
@@ -1,1 +1,18 @@
+package com.example.demo.EmployeeManagement.service;
+
+import java.util.List;
+
+import com.example.demo.EmployeeManagement.Entity.Attendence;
+
+public interface AttendenceService {
+
+	void save(Attendence attendence);
+
+	List<Attendence> getAllattendence();
+    
+	Attendence updateattendence(Attendence attendence, Integer id);
+
+	Attendence getById(Integer id);
+
+		}
 
