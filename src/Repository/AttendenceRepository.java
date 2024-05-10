1521191f68e9a3a3fa60decad0248200af2330c9
PoornimaKarma <161825445+PoornimaKarma@users.noreply.github.com>
2024-05-10 16:40:26 +0530

    Update AttendenceRepository.java

--- src/Repository/AttendenceRepository.java
+++ src/Repository/AttendenceRepository.java
@@ -1,1 +1,16 @@
+package com.example.demo.EmployeeManagement.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.EmployeeManagement.Entity.Attendence;
+
+@Repository
+public interface AttendenceRepository extends JpaRepository<Attendence, Integer>
+{
+
+
+
+	
+}
 
